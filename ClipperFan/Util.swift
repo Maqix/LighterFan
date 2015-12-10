fb2322471bfcf3076e1f18c76ba8a907a1e4d212
Maqix <stincodisanto@gmail.com>
2015-12-10 18:22:55 +0100

    Aggiunto l'algoritmo per generare una immagine unica da un array di immagine

--- ClipperFan/Util.swift
+++ ClipperFan/Util.swift
@@ -77,4 +77,66 @@ class Util: NSObject
             // pass back new image
             return outImage
     }
+    
+    
+    func stitchImages(images: [UIImage], isVertical: Bool) -> UIImage {
+        var stitchedImages : UIImage!
+        if images.count > 0 {
+            var maxWidth = CGFloat(0), maxHeight = CGFloat(0)
+            for image in images {
+                if image.size.width > maxWidth {
+                    maxWidth = image.size.width
+                }
+                if image.size.height > maxHeight {
+                    maxHeight = image.size.height
+                }
+            }
+            var totalSize : CGSize, maxSize = CGSizeMake(maxWidth, maxHeight)
+            if isVertical {
+                totalSize = CGSizeMake(maxSize.width, maxSize.height * (CGFloat)(images.count))
+            } else {
+                totalSize = CGSizeMake(maxSize.width  * (CGFloat)(images.count), maxSize.height)
+            }
+            UIGraphicsBeginImageContext(totalSize)
+            for image in images {
+                var rect : CGRect, offset = (CGFloat)((images as NSArray).indexOfObject(image))
+                if isVertical {
+                    rect = CGRectMake(0, maxSize.height * offset, maxSize.width, maxSize.height)
+                } else {
+                    rect = CGRectMake(maxSize.width * offset, 0 , maxSize.width, maxSize.height)
+                }
+                image.drawInRect(rect)
+            }
+            stitchedImages = UIGraphicsGetImageFromCurrentImageContext()
+            UIGraphicsEndImageContext()
+        }
+        return stitchedImages
+    }
+    
+    func immagineQuadrataFromArray(images: [UIImage]) -> UIImage
+    {
+        var arrayRighe = [UIImage]()
+        let numeroImmagini = images.count
+        //Il lato viene decrementato di 1 perchè gli array partono da 0
+        let lato = Int(sqrt(Float(numeroImmagini))) - 1
+        
+        //Costruisco l'array di righe
+        var partenza = 0
+        for i in 0...lato
+        {
+            print("Giro \(i): Partenza = \(partenza) Partenza+Lato = \(partenza+lato)")
+            let immaginiRiga = images[partenza...partenza+lato]
+            let riga = stitchImages(Array(immaginiRiga), isVertical: false)
+            arrayRighe.append(riga)
+            if (partenza%lato==0)
+            {
+                partenza += lato
+            }
+        }
+        
+        //Unisco le righe verticalmente per ottenere una matrice
+        let matrice = stitchImages(arrayRighe, isVertical: true)
+        
+        return matrice
+    }
 }
